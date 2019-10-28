cea0637d9cfdc53529adb9e2aff4cb7092a334c6
Mahmoud Nafea <mahmoudnafea19@gmail.com>
2019-10-28 23:48:29 +0800

    fetching data from the backend

--- client/src/components/teams/teams.js
+++ client/src/components/teams/teams.js
@@ -1,18 +1,27 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './teams.css'
 
 const Teams = () => {
-    const [showTeams, setShowTeams] = useState(null)
-    useEffect(() => {
-        fetch('/').then(res => res.json()).then(showTeams => setShowTeams({ showTeams }, () => {
-            console.log('data fetch ', showTeams)
-        }))
-    })
+    const [data, setData] = useState([])
+
+    useEffect(async () => {
+        const response = await fetch('https://apiv2.apifootball.com/?action=get_standings&league_id=148&APIkey=86426c746697bc6f1c2a765d94bf4891adcea52800c28be9945573f0c75bf77c')
+        const fetchedData = await response.json()
+        setData(fetchedData)
+    }, [])
     return (
         <div>
-            <title>Fullstack app</title>
             <h1>Premier league teams</h1>
-        </div>
+
+            <ul>
+                {data.map((item) => (
+
+                    <li key={item.team_id}>{item.team_name} Rank: {item.overall_league_position}</li>
+                ))}
+            </ul>
+
+        </div >
     )
 }
 
