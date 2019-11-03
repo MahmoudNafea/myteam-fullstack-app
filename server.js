2831e9479427983097228f11bd07e7205f71787a
Mahmoud Nafea <mahmoudnafea19@gmail.com>
2019-11-03 23:28:53 +0800

    linking front end and back end

--- server.js
+++ server.js
@@ -3,16 +3,14 @@ const request = require('request')
 
 
 const server = express()
-// const BASE_URL = process.en.BASE_URL
-// const LEAGUE_ID = process.en.LEAGUE_ID
-// const API_KEY = process.en.API_KEY
+const BASE_URL = process.env.BASE_URL
+const LEAGUE_ID = process.env.LEAGUE_ID
+const API_KEY = process.env.API_KEY
 
 server.get('/', (req, res) => {
 
-    const url = 'https://apiv2.apifootball.com/?action=get_standings&league_id=148&APIkey=86426c746697bc6f1c2a765d94bf4891adcea52800c28be9945573f0c75bf77c'
-    // const url = `${BASE_URL}?action=get_standings&league_id=${LEAGUE_ID}&APIkey=${API_KEY}`
+    const url = `${BASE_URL}?action=get_standings&league_id=${LEAGUE_ID}&APIkey=${API_KEY}`
     request(url, (error, body) => {
-        // console.log('body: ', body)
         res.send(JSON.parse(body.body))
     })
 })
